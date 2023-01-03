6555e471d24dce08a9c50d60736061214badc358
jordan-verschoor <JBLDSKY@gmail.com>
2023-01-03 18:38:59 +0100

    New button~
    Now press '1' to click on a button that says 'OK'!

--- submitter.js
+++ submitter.js
@@ -1,14 +1,7 @@
 // initialization message
 console.log("Submitter.js loaded");
 
-//script runs on keypress
-document.addEventListener("keypress", (e) => {
-  // debuggin
-  // console.log($("button"));
-
-  // jquery to get all buttons
-  const buttons = $("button");
-
+const click_submit = (buttons) => {
   // loop through all buttons on the page
   for (let i = 0; i < buttons.length; i++) {
     if (buttons[i].innerHTML === "Submit") {
@@ -16,5 +9,37 @@ document.addEventListener("keypress", (e) => {
       buttons[i].click();
       break;
     }
+  }
+};
+
+const click_ok = (buttons) => {
+  // loop through all buttons on the page
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerHTML === "OK") {
+      // if the button is a submit button, click it
+      buttons[i].click();
+      break;
+    }
+  }
+};
+
+//script runs on keypress
+document.addEventListener("keypress", (e) => {
+  // debuggin
+  // console.log($("button"));
+
+  // jquery to get all buttons
+  const buttons = $("button");
+  console.log(buttons)
+
+  // switch case for keypresses
+  switch (e.key) {
+    case "`":
+      click_submit(buttons);
+      break;
+    case "1":
+      click_ok(buttons);
+    case "Enter":
+      break;
   }
 });
