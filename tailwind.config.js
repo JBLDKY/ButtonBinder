9adb29d1470639e5c016b54c1e7b818972a64a9c
jordan-verschoor <jbldsky@gmail.com>
2023-01-08 11:03:15 +0100

    Styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      "firefox-bg": "#25212b",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
